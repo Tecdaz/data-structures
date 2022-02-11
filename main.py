14e5012b295aaef2b3503ad1fc964fe745b695da
Santiago <santi99oca@gmail.com>
2022-02-11 03:35:30 -0300

    Quick sort

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from random import randint
 
-from sort import bubble_sort, insertion_sort, selection_sort
+from sort import quick_sort
 
 if __name__ == '__main__':
-    array = [randint(1, 1000) for i in range(20)]
-    selection_sort(array)
+    array = [randint(1, 1000) for i in range(10)]
+    quick_sort(array)
     print(array)
-
-
-
-
--- sort.py
+++ sort.py
@@ -1,34 +1,56 @@
-def bubble_sort(array):
-    max_length = len(array) - 1
+def bubble_sort(arr):
+    max_length = len(arr) - 1
     for i in range(max_length):
         # Asumimos que esta ordenado para no hacer recorridos innesesarios
         sort = True
         for j in range(max_length - i):
-            if array[j] > array[j + 1]:
+            if arr[j] > arr[j + 1]:
                 # Significa que falta ordenar
                 sort = False
-                array[j], array[j + 1] = array[j + 1], array[j]
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
         # Se cumple siempre y cuando no se haya ordenado nada
         if sort:
             break
 
 
-def insertion_sort(array):
+def insertion_sort(arr):
     i = 1
-    while i < len(array):
+    while i < len(arr):
         j = i
-        while (j > 0) and (array[j] < array[j - 1]):
-            array[j], array[j - 1] = array[j - 1], array[j]
+        while (j > 0) and (arr[j] < arr[j - 1]):
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
             j -= 1
         i += 1
 
 
-def selection_sort(array):
-    max_length = len(array)
+def selection_sort(arr):
+    max_length = len(arr)
     for i in range(max_length):
         # Encuentra el valor minimo desde la i
-        min_value = min(array[i:])
-        if min_value != array[i]:
+        min_value = min(arr[i:])
+        if min_value != arr[i]:
             # ubica el index del nuevo minimo y los intercambia
-            new_index = array[i:].index(min_value) + i
-            array[i], array[new_index] = array[new_index], array[i]
+            new_index = arr[i:].index(min_value) + i
+            arr[i], arr[new_index] = arr[new_index], arr[i]
+
+
+def quick_sort(arr):
+    def partition(start, end, array):
+        pivot = array[end]
+        i = start
+        for j in range(start, end):
+            if array[j] <= pivot:
+                array[i], array[j] = array[j], array[i]
+                i += 1
+
+        array[i], array[end] = array[end], array[i]
+
+        return i
+
+    def recursion(start, end, array):
+        if start < end:
+            p = partition(start, end, array)
+            recursion(start, p - 1, array)
+            recursion(p + 1, end, array)
+
+    recursion(0, len(arr) - 1, arr)
