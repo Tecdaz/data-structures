93a019f5b8b3e4945441a7e49e21c3e7f3807b2c
Santiago <santi99oca@gmail.com>
2022-02-11 00:53:29 -0300

    insertion sort and selection sort

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from random import randint
 
-from sort import bubble_sort
+from sort import bubble_sort, insertion_sort, selection_sort
 
 if __name__ == '__main__':
-    array = [randint(1, 1000) for i in range(21)]
-    bubble_sort(array)
+    array = [randint(1, 1000) for i in range(20)]
+    selection_sort(array)
     print(array)
+
+
+
+
--- sort.py
+++ sort.py
@@ -11,3 +11,24 @@ def bubble_sort(array):
         # Se cumple siempre y cuando no se haya ordenado nada
         if sort:
             break
+
+
+def insertion_sort(array):
+    i = 1
+    while i < len(array):
+        j = i
+        while (j > 0) and (array[j] < array[j - 1]):
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
+        i += 1
+
+
+def selection_sort(array):
+    max_length = len(array)
+    for i in range(max_length):
+        # Encuentra el valor minimo desde la i
+        min_value = min(array[i:])
+        if min_value != array[i]:
+            # ubica el index del nuevo minimo y los intercambia
+            new_index = array[i:].index(min_value) + i
+            array[i], array[new_index] = array[new_index], array[i]
